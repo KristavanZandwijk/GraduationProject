5bbe383f0f6c47be9a48da5e9f85a035e53bae0b
Krista van Zandwijk <g.r.k.v.zandwijk@student.tue.nl>
2024-09-02 16:26:40 +0200

    IFC Viewer new attempt

--- client/src/scenes/DataPrivacy/index.jsx
+++ client/src/scenes/DataPrivacy/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import Header from 'components/Header';
+import IFCViewer3 from 'components/IFCViewer3';
 
 const DataPrivacy = () => {
-
   return (
-    <Box m="1.5rem 2.5rem" height="100vh" display="flex">
-      {/* Right side content */}
-      <Box flex="1" ml={4}>
-        <Header
-          title="Data Privacy"
-          subtitle="This page will show what data is shared with whom."
-        />
+    <Box m="1.5rem 2.5rem">
+      {/* Header at the top */}
+      <Header
+        title="Data Privacy"
+        subtitle="This page will show what data is shared with whom."
+      />
+      
+      {/* Viewer under the header */}
+      <Box mt={4} height="80vh">
+        <IFCViewer3 />
       </Box>
     </Box>
   );
